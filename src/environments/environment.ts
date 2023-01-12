f19a88c7806470e9032f27523b236b5f2b50e366
tutos69 <edwinz-2@outlook.com>
2023-01-12 08:11:10 -0500

    Firebase-Ionic2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAtRZZLDHbKYTu3-j2nMjIN9hbKEbqn6jE",
+    authDomain: "ionic-2-c9ab3.firebaseapp.com",
+    projectId: "ionic-2-c9ab3",
+    storageBucket: "ionic-2-c9ab3.appspot.com",
+    messagingSenderId: "690489122930",
+    appId: "1:690489122930:web:5b0413de03e5ea60b662eb",
+    measurementId: "G-W0F6BWYVW6"
+  }
 };
 
 /*
